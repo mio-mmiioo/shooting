7994df5ec1a74eb6354e5efdffb0b1f96bce8d2e
240672 <240672@E0403-13>
2025-12-11 09:22:30 +0900

    DestructibleObjectが表示されなかった不具合を修正

--- Source/PlayScene/GameMaster.cpp
+++ Source/PlayScene/GameMaster.cpp
@@ -45,7 +45,7 @@ void GameMaster::Init()
 {
 	WayInfo::Init();
 	//Area::SetStage(); // 最初のステージをセット
-	new Stage(0); // 建物邪魔な場合のステージ
+	new Stage(5); // 建物邪魔な場合のステージ
 	player = FindGameObject<Player>();
 	enemy = FindGameObjects<Enemy>();
 	stage = FindGameObject<Stage>();
